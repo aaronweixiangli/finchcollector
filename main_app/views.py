f704e7726ce2449acedcc1b6437869e30b4b7f4b
aaronweixiangli <aaronweixiangli@gmail.com>
2023-03-02 17:48:52 -0800

    Toy&Photo models

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
+import uuid
+import boto3
+import os
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .forms import FeedingForm
-from .models import Finch
+from .models import Finch, Toy, Photo
 
 # Create your views here.
 def home(request):
@@ -18,15 +22,18 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    id_list = finch.toys.all().values_list('id')
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
     feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {
         'finch': finch,
         'feeding_form': feeding_form,
+        'toys': toys_finch_doesnt_have
     })
 
 class FinchCreate(CreateView):
     model = Finch
-    fields = '__all__'
+    fields = ['name', 'species', 'description', 'age']
 
 class FinchUpdate(UpdateView):
     model = Finch
@@ -44,3 +51,47 @@ def add_feeding(request, finch_id):
         new_feeding.save()
     return redirect('detail', finch_id = finch_id)
 
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys'
+
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def add_photo(request, finch_id):
+  # photo-file maps to the "name" attr on the <input>
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # Need a unique "key" (filename)
+    # It needs to keep the same file extension
+    # of the file that was uploaded (.png, .jpeg, etc)
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      bucket = os.environ['S3_BUCKET']
+      s3.upload_fileobj(photo_file, bucket, key)
+      url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+      Photo.objects.create(url=url, finch_id=finch_id)
+    except Exception as e:
+      print('An error occurred uploading file to S3')
+      print(e)
+  return redirect('detail', finch_id=finch_id)
